7c5dea2603d1692bfe792e5fd90475fd224e92f0
akshayb407 <akshayb407@gmail.com>
2022-08-12 19:16:29 +0530

    .gitignore

---
+++ LinkedLists/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("======UC1========");
+            List<int> list = new List<int>();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            Console.WriteLine(list);
+        }
+    }
+}
